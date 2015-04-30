f6853a389f198bab76de542d7ec424ef332b6d14
Emily Zhao <emily@dropbox.com>
2015-04-30 14:54:25 -0700

    use ProgressWheel and Timer for a basic stopwatch

--- app/src/main/java/com/herokuapp/ezhao/timer/MainActivity.java
+++ app/src/main/java/com/herokuapp/ezhao/timer/MainActivity.java
@@ -2,15 +2,67 @@ package com.herokuapp.ezhao.timer;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.todddavies.components.progressbar.ProgressWheel;
+import java.util.Timer;
+import java.util.TimerTask;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MainActivity extends ActionBarActivity {
+    @InjectView(R.id.pwHabitProgress) ProgressWheel pwHabitProgress;
+    private enum SpinStatus {
+        STOPPED, INDEFINITE, GOAL_SET
+    }
+    SpinStatus spinStatus;
+    long startTime;
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
+        spinStatus = SpinStatus.STOPPED;
+        pwHabitProgress.setProgress(0);
+    }
+
+    @OnClick(R.id.pwHabitProgress)
+    public void onWheelTap(View view) {
+        final ProgressWheel progressWheel = (ProgressWheel) view;
+
+        if (spinStatus == SpinStatus.INDEFINITE) {
+            spinStatus = SpinStatus.STOPPED;
+            pwHabitProgress.stopSpinning();
+            if (timer != null) {
+                timer.cancel();
+            }
+        } else {
+            spinStatus = SpinStatus.INDEFINITE;
+            pwHabitProgress.spin();
+            startTime = System.currentTimeMillis();
+
+            if (timer != null) {
+                timer.cancel();
+            }
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    int diff = (int) (System.currentTimeMillis() - startTime);
+                    int hundredths = (diff / 10) % 100;
+                    int seconds = (diff / 1000) % 60;
+                    int minutes = (diff/ (1000 * 60)) % 60;
+                    progressWheel.setText(String.format("%02d:%02d:%02d", minutes, seconds, hundredths));
+                }
+            }, 0, 9);
+        }
     }
 
     @Override
